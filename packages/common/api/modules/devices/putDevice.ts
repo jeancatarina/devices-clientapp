d8ca860898a5140bb69c39cf275ad6537c120c4e
jeancatarina <jcc_ptb@hotmail.com>
2023-01-23 15:42:17 -0300

    fix: add put id

--- packages/common/api/modules/devices/putDevice.ts
+++ packages/common/api/modules/devices/putDevice.ts
@@ -2,4 +2,5 @@ import { Device } from '@devices/types'
 
 import { axiosCall } from '../../services/axiosCall'
 
-export const putDevice = (payload: Device) => axiosCall.put('/devices', payload)
+export const putDevice = (payload: Device) =>
+  axiosCall.put(`/devices/${payload.id}`, payload)
